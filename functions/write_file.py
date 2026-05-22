ce55b4172320760f86a877976bde4405c91bd107
j3arch <j3archx@gmail.com>
2026-05-22 23:04:15 +0300

    added type hints

--- functions/write_file.py
+++ functions/write_file.py
@@ -1,7 +1,7 @@
 import os
 from google.genai import types
 
-def write_file(working_directory, file_path, content):
+def write_file(working_directory: str, file_path: str, content: str) -> str:
     abs_working_dir = os.path.abspath(working_directory)
     abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
     common = os.path.commonpath([abs_working_dir, abs_file_path])
